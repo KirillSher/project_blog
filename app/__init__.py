477176192fa181966be2f9364b481bdea01e9908
KirillSher <SHERSNE@yandex.ru>
2024-11-09 00:07:53 +0300

    v0.0.1

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 
 from app.routes.auth import login, auth_bp
-from app.routes import auth, home, profile, admin, posts
+from app.routes import auth, home, profile, admin
 app.register_blueprint(auth_bp, url_prefix='/auth')  # Все маршруты будут начинаться с /auth
 
 app.config.from_object(Config)
